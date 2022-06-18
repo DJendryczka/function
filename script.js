9b9123165ad6f80570826c6b64289ee8d0de8b17
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-06-18 17:44:57 +0200

    Refacores code

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 
-// Funkcja getNumber szuka i lapie elememt z id `number` linia 17 i wklada do input1
+
 function getNumber(){
-    const input1 = document.querySelector('input').value;
-    console.log(input1);
-    // stala sizeOffCircle to funkcja ktora wykorzystuje input1 i oblicza i wyzuca wynik
-    const sizeOffCircle = function(input1){
-        return 3.14 * input1;
-        
-
-    };
-    // stala size1 uruchamia funkcie sizeOffCircle i przechowuje wynik obliczen funkcji
-    const size1 = sizeOffCircle(input1);
-
-    // linia 13 wpisuje wynik ze stalej size1 do elementu p linia 20 HTML
-    document.getElementById("demo").innerHTML = size1;
+    let input1 = document.querySelector('input').value;
+    let outPut = input1 * 3.14
+    document.getElementById("demo").innerHTML = outPut
 }
 
 
